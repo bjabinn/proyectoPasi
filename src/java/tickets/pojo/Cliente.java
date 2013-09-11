d76c989d1a12306269f5ee728380c8454c9cf64f
bjabinn <bjabinnn@gmail.com>
2013-09-11 18:06:06 +0200

    Update Cliente.java

--- src/java/tickets/pojo/Cliente.java
+++ src/java/tickets/pojo/Cliente.java
@@ -1,3 +1,17 @@
+package tickets.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+
+
 @Entity
 public class Cliente {
 
